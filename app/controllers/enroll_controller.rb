b3a6d422f48cd30a5864fe55a9612128964c6afe
MOSapeizer <k471352@gmail.com>
2016-04-27 14:47:39 +0800

    project init

---
+++ app/controllers/enroll_controller.rb
@@ -0,0 +1,2 @@
+class EnrollController < ApplicationController
+end
---
+++ test/controllers/enroll_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EnrollControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
